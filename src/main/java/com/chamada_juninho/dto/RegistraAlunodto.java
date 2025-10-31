7c020e24b84460b394d8cccae9f426fff16df266
thicp <thicp10@outlook.com>
2025-10-31 00:11:43 -0300

    criado a configuracao do banco

--- src/main/java/com/chamada_juninho/dto/RegistraAlunodto.java
+++ src/main/java/com/chamada_juninho/dto/RegistraAlunodto.java
@@ -11,25 +11,30 @@ import lombok.*;
 public class RegistraAlunodto {
     private long id;
     private String nome;
+    private boolean inativo;
     private Long telefone;
     private String matricula;
     private byte idade;
     private String declaracao;
     private String escola;
+    private String periodo;
     private String responsavel;
     private String rgResponsavel;
     private String rgCrianca;
     private String cpfCrianca;
+    private static String observacao;
 
     public static RegistraAlunodto fromEntity(RegistraAlunos entity) {
         return RegistraAlunodto.builder()
                 .id(entity.getId())
+                .inativo(entity.isinativo(observacao))
                 .nome(entity.getNome())
                 .telefone(entity.getTelefone())
                 .matricula(entity.getMatricula())
                 .idade(entity.getIdade())
                 .declaracao(entity.getDeclaracao())
                 .escola(entity.getEscola())
+                .periodo(entity.getPeriodo())
                 .responsavel(entity.getResponsavel())
                 .rgResponsavel(entity.getRgResponsavel())
                 .rgCrianca(entity.getRgCrianca())
@@ -40,12 +45,14 @@ public class RegistraAlunodto {
     public RegistraAlunos toEntity() {
         return RegistraAlunos.builder()
                 .id(this.id)
+                .inativo(this.inativo)
                 .nome(this.nome)
                 .telefone(this.telefone)
                 .matricula(getMatricula())
                 .idade(this.idade)
                 .declaracao(this.declaracao)
                 .escola(this.escola)
+                .periodo(this.periodo)
                 .responsavel(this.responsavel)
                 .rgResponsavel(this.rgResponsavel)
                 .rgCrianca(this.rgCrianca)
--- src/main/java/com/chamada_juninho/entity/RegistraAlunos.java
+++ src/main/java/com/chamada_juninho/entity/RegistraAlunos.java
@@ -15,31 +15,40 @@ public class RegistraAlunos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private boolean inativo;
     private String nome;
     private Long telefone;
     private String matricula;
     private byte idade;
     private String declaracao;
     private String escola;
+    private String periodo;
     private String responsavel;
     private String rgResponsavel;
     private String rgCrianca;
     private String cpfCrianca;
+    private String observacao;
 
 
     public RegistraAlunos(long id, String nome, Long telefone) {
         this.id = id;
+        this.inativo = isinativo(observacao);
         this.nome = nome;
         this.telefone = telefone;
         this.matricula = null;// Inicializa matricula como null
         this.idade =0;
         this.declaracao = null;
         this.escola = null;
+        this.periodo = null;
         this.responsavel = null;
         this.rgResponsavel = null;
         this.rgCrianca = null;
         this.cpfCrianca = null;
+        this.observacao = null;
     }
 
+    public boolean isinativo(String observacao) {
+    return this.observacao != null && this.observacao.toLowerCase().contains("inativo");
+    }
 }
 
